76b4dc6c4b9ff62958fb01eabb044f8c5b5bd159
Roberto Ruiz <rruiz@est.frba.utn.edu.ar>
2018-07-30 03:45:48 -0300

    Gitignore y primeros pasos

---
+++ server/Controllers/ItemsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        // GET /api/items?q=:query
+        [HttpGet(Name = nameof(GetItemsByQuery))]
+        public async Task<IActionResult> GetItemsByQuery([FromQuery]string q)
+        {
+            return Ok(q);
+        }
+        
+        // GET /api/items/:id
+        [HttpGet]
+        [Route("{id}", Name = nameof(GetItemById))]
+        public async Task<IActionResult> GetItemById(string id)
+        {
+            return Ok(id);
+        }
+    }
+}
