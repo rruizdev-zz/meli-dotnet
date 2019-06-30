5a726be4f3e848dbd5fea60c1b64270df5b5609b
Roberto Ruiz <rruiz@est.frba.utn.edu.ar>
2019-06-30 01:54:46 -0300

    Upgrade to .NET Core v2.2

---
+++ backend/MercadoLibre.Backend/MercadoLibre.Backend.WebApi/Controllers/ItemsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MercadoLibre.Backend.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetItemsLike(string q)
+        {
+            return new string[] { q };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> GetItemDetail(int id)
+        {
+            return $"{id}: value";
+        }
+    }
+}
