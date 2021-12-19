7ba60ad0a6ce38a30310a45b9942bbe606f61bdc
Roberto Ruiz <roberto.ruiz01@globant.com>
2021-12-19 03:31:56 -0300

    Remove Startup.cs

--- src/Meli.Backend.Api/Program.cs
+++ src/Meli.Backend.Api/Program.cs
@@ -1,15 +1,57 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using Microsoft.AspNetCore;
+﻿using AutoMapper;
+using MediatR;
+using Meli.Backend.Api;
+using Meli.Backend.Application.Mappers;
+using Meli.Backend.Application.Services;
+using Meli.Backend.Application.Services.Interfaces;
 
-namespace Meli.Backend.Api
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient<IItemsService, ItemsService>();
+
+builder.Services.AddSingleton(new MapperConfiguration(config =>
 {
-    [ExcludeFromCodeCoverage]
-    public class Program
+    config.AddProfile(new ItemMapper());
+}).CreateMapper());
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("_corsOrigins", builder =>
     {
-        public static void Main(string[] args) => 
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build()
-                .Run();
-    }
+        builder.WithOrigins("http://localhost:1810");
+    });
+});
+
+builder.Services.AddMediatR(typeof(Program));
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
+
+//    config.SchemaGeneratorOptions = document =>
+//    {
+//        document.Info.Version = "v1";
+//        document.Info.Title = "meli-dotnet";
+//        document.Info.Description = "ASP.NET Core Web API works with external API";
+//        document.Info.TermsOfService = "None";
+//        document.Info.Contact = new OpenApiContact
+//        {
+//            Name = "Roberto Ruiz",
+//            Url = "https://linktr.ee/rruizdev"
+//        };
+//    };
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseCors("_corsOrigins");
+
+app.UseSwagger();
+
+app.UseSwaggerUI();
+
+app.Run();
